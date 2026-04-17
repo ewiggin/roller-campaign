7c62996ebf355ffca4186cb08e1cfaa6513c1db2
Mario Martinez <mmartinez@energy-tools.net>
2026-04-17 21:23:02 +0200

    delete all deno configurations instead spa

--- server.ts
+++
@@ -1,9 +0,0 @@
-import { serveDir } from "jsr:@std/http/file-server";
-
-Deno.serve({ port: 8000 }, (req) => {
-  return serveDir(req, {
-    fsRoot: "dist/form-guests/browser",
-    urlRoot: "",
-    quiet: true,
-  });
-});
