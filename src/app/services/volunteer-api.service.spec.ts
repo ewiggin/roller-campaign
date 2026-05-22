ea46a60bbaf84afb3d87b7518e01bc390d6fc1bb
David P <taborlindp@gmail.com>
2026-05-22 13:37:26 +0200

    fix failing test

---
+++ src/app/services/volunteer-api.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { VolunteerApiService } from './volunteer-api.service';
+
+describe('VolunteerApiService', () => {
+  let service: VolunteerApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+    service = TestBed.inject(VolunteerApiService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
