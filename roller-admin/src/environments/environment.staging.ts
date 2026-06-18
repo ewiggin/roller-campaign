f5ef94e921c47ec21bbc4d60845044e2392b7bf9
Mario Martinez <mario.martinez@somenergia.coop>
2026-06-18 23:42:41 +0200

    🔖bump to 0.2.7

--- roller-admin/src/environments/environment.staging.ts
+++ roller-admin/src/environments/environment.staging.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: 'https://roller-api-staging.up.railway.app/api',
   googleMapsApiKey: 'AIzaSyCAlxhBc7f3JB3dcUtamk_vka3VGGccOlQ',
-  version: '0.2.6',
+  version: '0.2.7',
 };
